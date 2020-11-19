beaa981582118384338dbd8bb593da9717ba43cd
René Pacios <rpacios@webrene.es>
2020-11-19 19:44:34 +0100

    added build props and config project dependencies

--- samples/Net.Core.Sample/Models/TableWithHeaders.cs
+++ samples/Net.Core.Sample/Models/TableWithHeaders.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ClosedXML.TableReader;
-using ClosedXML.TableReader.Attributes;
 
 namespace Net.Core.Sample.Models
 {
